bc3df12bc596a18bb03cc9cd30d26bccbd74c398
Руслан Галяудинов <rus4855@yandex.ru>
2026-03-28 12:07:32 +0300

    Созданы файлы db_manager.py, hh_api.py, utils.py

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from src.hh_api import HeadHunterAPI
+
+
+def get_company_vacancy(companies: list[str]) -> list[dict[str, Any]]:
+    """Получение данных о работодателях и их вакансиях"""
+
+    hh_api = HeadHunterAPI()
+    data = []
+    for company in companies:
+        company_data = hh_api.get_employer_info(company)
+        all_vacancy = hh_api.get_employer_vacancies(company_data["id"])
+
+        if not all_vacancy:
+            print(f"Не удалось получить вакансии для id компании : {company}")
+            all_vacancy = []
+
+        data.append({"company": company_data, "vacancy": all_vacancy})
+
+    return data
